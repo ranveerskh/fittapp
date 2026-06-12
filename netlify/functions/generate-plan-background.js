5a53e646065d2e894f9de1c12773778d33d5b45a
ranveerskh <ranveerskh@gmail.com>
2026-06-12 14:24:08 -0400

    Increase maxOutputTokens for Premium Plus plan

--- netlify/functions/generate-plan-background.js
+++ netlify/functions/generate-plan-background.js
@@ -49,7 +49,7 @@ PREMIUM PLAN RULES:
     generationType: "premium_plus_weekly",
     isPremium: true,
     depth: "advanced",
-    maxOutputTokens: 7600,
+    maxOutputTokens: 18000,
     planInstruction: `
 PREMIUM PLUS PLAN RULES:
 - Create the strongest version of the plan.
